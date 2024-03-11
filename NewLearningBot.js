cabb582957e5d4e038e541125d04a6f7374f9274
ChepkoechDorcasSang <dorcas41975173@gmail.com>
2024-03-11 21:43:47 +0300

    first commit

---
+++ NewLearningBot.js
@@ -0,0 +1,9 @@
+function generateNumber(inputNumber){
+    for(let i=1; i<=inputNumber; i++){
+        let pattern=;
+        for(let j=1;j<=i;j++){
+            pattern +=i;
+        }
+        console.log(pattern);
+    }
+}
