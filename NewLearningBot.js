513fc6af863674bc4c8f99bd88235adb824c4440
ChepkoechDorcasSang <dorcas41975173@gmail.com>
2024-03-12 11:42:07 +0300

    first commit

--- NewLearningBot.js
+++ NewLearningBot.js
@@ -1,9 +1,10 @@
 function generateNumber(inputNumber){
     for(let i=1; i<=inputNumber; i++){
-        let pattern=;
+        let pattern="";
         for(let j=1;j<=i;j++){
             pattern +=i;
         }
         console.log(pattern);
     }
-}
+}generateNumber(5)
+
